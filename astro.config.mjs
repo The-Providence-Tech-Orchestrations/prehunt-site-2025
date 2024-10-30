13c9d56b31f29b041b4033c4bc595652f3ccfe3e
Orion Bloomfield <obloomfi@amazon.com>
2024-10-30 08:36:40 -0700

    landing with countdown and info

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,9 @@ import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
 
+import icon from "astro-icon";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react(), mdx()],
+  integrations: [tailwind(), react(), mdx(), icon()],
 });
