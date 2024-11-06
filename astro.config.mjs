77f61150a4728285cb2d66ae2187baafa15fa353
Orion Bloomfield <32281675+obloomfield@users.noreply.github.com>
2024-11-05 18:09:30 -0800

    Update astro.config.mjs for custom domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,5 @@ import icon from "astro-icon";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react(), mdx(), icon()],
-  site: 'https://the-providence-tech-orchestrations.github.io',
-  base: 'prehunt-site-2025',
+  site: 'http://www.mitmysteryheist.com',
 });
