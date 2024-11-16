c460afbb515cbad8b34f6286d1e2ed75642bf52a
Hammad Izhar <hizhar@g.harvard.edu>
2024-11-16 18:48:59 -0500

    feat: add story

--- src/components/react/ModePicker.tsx
+++ src/components/react/ModePicker.tsx
@@ -1,13 +1,15 @@
 import { useLocalStorage } from "@uidotdev/usehooks";
 
-import InfoDialog from "./InfoDialog";
 import ModePickDialog from "./ModePickDialog";
+import StoryDialog from "./StoryDialog";
 
 function ModePicker() {
   const [isExpert, setisExpert] = useLocalStorage("puzzle-mode", true);
 
   return (
     <div className="flex justify-center items-center gap-2 py-4 space-x-2">
+      <StoryDialog />
+      <div>|</div>
       <p className={`font-bold ${!isExpert && "blur-[0.5px]"}`}>Casual</p>
       <input
         className="appearance-none w-14 h-6 rounded-full relative cursor-pointer outline-none transition-all duration-200 ease-in-out bg-gray-300 checked:bg-[#63268a] after:content-[''] after:absolute after:w-6 after:h-6 after:rounded-full after:bg-white after:left-0 after:transition-all after:duration-200 after:ease-in-out after:checked:left-[calc(100%_-_1.5rem)]"
---
+++ src/components/react/StoryDialog.tsx
@@ -0,0 +1,40 @@
+import { useLocalStorage } from "@uidotdev/usehooks";
+import { useEffect } from "react";
+
+import art from "@/assets/art/story.png";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+
+const StoryDialog = () => {
+  const [hasSeenStory, setHasSeenStory] = useLocalStorage("has-seen-story", false);
+
+  useEffect(() => {
+    if (!hasSeenStory) {
+      setHasSeenStory(true);
+    }
+  }, []);
+
+  return (
+    <Dialog defaultOpen={!hasSeenStory}>
+      <DialogTrigger className="rounded-full border border-zinc-400 transition-all blur-[0.5px] hover:border-zinc-800">
+        <div className="flex px-3 py-1 text-center">📖</div>
+      </DialogTrigger>
+      <DialogContent className="dark bg-[black]">
+        <DialogHeader>
+          {/* <DialogTitle className="pt-4">THE STORY</DialogTitle> */}
+          <DialogDescription className="text-white blur-[0.75px] text-left">
+            <img src={art.src} alt="Story" />
+          </DialogDescription>
+        </DialogHeader>
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default StoryDialog;
