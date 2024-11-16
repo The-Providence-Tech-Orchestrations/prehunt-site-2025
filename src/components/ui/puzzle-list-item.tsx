5f679078154931c3d8fd23421bbc9ca040e60964
Hammad Izhar <hizhar@g.harvard.edu>
2024-11-16 02:02:05 -0500

    add rover-escape puzzle

--- src/components/ui/puzzle-list-item.tsx
+++
@@ -1,9 +0,0 @@
-interface PuzzleListItemProps {
-  slug: string;
-  title: string;
-  answer?: string;
-}
-
-export default function PuzzleListItem( {slug, title, answer }: PuzzleListItemProps) {
-  return <div><a href='{slug}'>{title}</a>{answer ? <div>answer</div> : ''}
-}
